761d67b497473b1c104a8c74fefa1862fd8db452
Abtahee Ali <abtaheeali@gmail.com>
2020-06-06 12:49:27 -0400

    Added option for user to create tamagotchi

--- Controllers/TamagotchisController.cs
+++ Controllers/TamagotchisController.cs
@@ -39,5 +39,44 @@ namespace LandOfTamagotchiAPI.Controllers
 
             return Ok(tamagotchi);
         }
+
+        [HttpPost]
+        public ActionResult<Tamagotchi> Create(Tamagotchi tamagotchiToCreate)
+        {
+            if (tamagotchiToCreate.HungerLevel != 0)
+            {
+                var errorMessage = new
+                {
+                    message = $"You requested hunger level to be at{tamagotchiToCreate.HungerLevel}, but each tamagotchi starts at a hunger level of 0. Please try again with the input of 0."
+                };
+
+                return UnprocessableEntity(errorMessage);
+            }
+
+            if (tamagotchiToCreate.HappinessLevel != 0)
+            {
+                var errorMessage = new
+                {
+                    message = $"You requested happiness level to be at{tamagotchiToCreate.HappinessLevel}, but each tamagotchi starts at a happiness level of 0. Please try again with the input of 0."
+                };
+
+                return UnprocessableEntity(errorMessage);
+            }
+
+            if (tamagotchiToCreate.Birthday != DateTime.Today)
+            {
+                var errorMessage = new
+                {
+                    message = $"You requested the tamagotchi's birthday to be at{tamagotchiToCreate.Birthday}, but each tamagotchi is given a birthday of the day it was created. Please try again."
+                };
+
+                return UnprocessableEntity(errorMessage);
+            }
+
+            _context.Tamagotchis.Add(tamagotchiToCreate);
+            _context.SaveChanges();
+
+            return CreatedAtAction(null, null, tamagotchiToCreate);
+        }
     }
 }
