43846ea05821c8a3b733402e93c9038592992443
Abtahee Ali <abtaheeali@gmail.com>
2020-06-07 16:40:02 -0400

    Fixed bug

--- Models/Tamagotchi.cs
+++ Models/Tamagotchi.cs
@@ -7,7 +7,7 @@ namespace LandOfTamagotchiAPI
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime Birthday { get; set; } = DateTime.Today;
-        public int HungerLevel { get; set; } = 0;
-        public int HappinessLevel { get; set; } = 0;
+        public int HungerLevel { get; set; }
+        public int HappinessLevel { get; set; }
     }
 }
